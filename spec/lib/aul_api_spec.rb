2518c700989627c96b800db58b70239626b4f7ac
Adam <atkolkma@gmail.com>
2014-06-24 17:02:34 -0400

    add more pending tests. Fix first test to expect Mechanize as superclass

--- spec/lib/aul_api_spec.rb
+++ spec/lib/aul_api_spec.rb
@@ -3,7 +3,11 @@ require 'aul_api'
 describe AutoUplink do
 
 	it "is a subclass of Mechanize" do
-		expect(AutoUplink.new.redirect_ok = true).to_not raise_exception
+		expect(AutoUplink.new).to be_a(Mechanize)
 	end
+
+	it "can log into AUL given working credentials"
+
+	it "can locate a vehicle, once logged in"
 
 end
