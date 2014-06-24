ea4d1875cda7d455961a569801f70ec2cebe7a41
Adam <atkolkma@gmail.com>
2014-06-24 17:08:25 -0400

    add tests to AutoUplink

--- spec/lib/aul_api_spec.rb
+++ spec/lib/aul_api_spec.rb
@@ -2,12 +2,20 @@ require 'aul_api'
 
 describe AutoUplink do
 
+	aul_scraper = AutoUplink.new
+
 	it "is a subclass of Mechanize" do
-		expect(AutoUplink.new).to be_a(Mechanize)
+		expect(aul_scraper).to be_a(Mechanize)
 	end
 
-	it "can log into AUL given working credentials"
+	it "can log into AUL given working credentials" do
+		expect(aul_scraper.login).not_to raise_error
+	end
 
-	it "can locate a vehicle, once logged in"
+	it "can locate a vehicle, once logged in" do
+		aul_scraper.login
+		expect(aul_scraper.find_vehicle(vehicle)).to_not raise_error
+	end
+
 
 end
