4f064479887f7d97e64e900a88d925bdd16a6f93
Adam <atkolkma@gmail.com>
2014-06-25 16:41:37 -0400

    basic login functionality and tests

--- lib/aul_api.rb
+++ lib/aul_api.rb
@@ -1,5 +1,34 @@
 require 'mechanize'
+require 'logger'
 
 class AutoUplink < Mechanize
+
+	@@login_url = "http://services.autouplinktech.com/login.cfm"
+	@@logout_url = "http://services.autouplinktech.com/logout.cfm"
+	@@main_menu_url = "http://services.autouplinktech.com/admin/mainoptions.cfm"
+
+	# Create a Mechanize agent
+	def initialize
+		super
+		@user_agent_alias = 'Mac Safari'
+		@follow_meta_refresh = true
+		@redirect_ok = true
+	end
+
+	# Login to Auto Uplink
+	def login (user, password)
+		begin
+		    self.get(@@login_url)
+		    form = self.page.forms.first
+		    form.uid = user
+		    form.pid = password
+		    self.submit(form)
+		rescue StandardError => e
+	    	logger = Logger.new(STDERR)
+	    	logger.error("Could not log into AutoUplink: #{e.message}")
+	    end
+	end
+
+	
 
 end
--- spec/lib/aul_api_spec.rb
+++ spec/lib/aul_api_spec.rb
@@ -1,23 +1,27 @@
 require 'aul_api'
 require 'yaml'
+
+# Gitignored. Use your own vehicles and Carfax credentials for testing!
 fixtures = YAML::load_file(File.join(__dir__, 'fixtures.yml'))
 credentials = YAML::load_file(File.join(__dir__, 'credentials.yml'))
 
+user_name = credentials["user_name"]
+password = credentials["password"]
+
 describe AutoUplink do
 
-	aul_scraper = AutoUplink.new
-
 	it "is a subclass of Mechanize" do
-		expect(aul_scraper).to be_a(Mechanize)
+		expect(AutoUplink.new).to be_a(Mechanize)
 	end
 
 	it "can log into AUL given working credentials" do
-		expect(aul_scraper.login).not_to raise_error
+		expect(AutoUplink.new.login(user_name, password).uri.to_s).to eq("http://services.autouplinktech.com/admin/mainoptions.cfm")
+		expect(AutoUplink.new.login("bad_username", "useless_password").uri.to_s).to eq("http://services.autouplinktech.com/login.cfm")
 	end
 
 	it "can locate a vehicle, once logged in" do
-		aul_scraper.login
-		expect(aul_scraper.find_vehicle(vehicle)).to_not raise_error
+		AutoUplink.new.login(user_name, password)
+		# expect(aul_scraper.find_vehicle(vehicle)).to_not raise_error
 	end
 
 
